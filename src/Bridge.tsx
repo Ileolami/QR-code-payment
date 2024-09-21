ee63d0ac949a87184126d79467940a12d01f815c
Ileolami <99335756+Ileolami@users.noreply.github.com>
2024-09-21 15:53:55 +0100

    Refactor Bridge.tsx for improved user experience and add vercel.json for routing configuration

--- src/Bridge.tsx
+++ src/Bridge.tsx
@@ -1,15 +1,16 @@
-// import { getArbitrumNetwork, EthBridger } from '@arbitrum/sdk'
-// import Web3, { eth, utils } from 'web3';
+import { getArbitrumNetwork, EthBridger } from '@arbitrum/sdk'
+import Web3, { eth, utils } from 'web3';
 
-// // get the `@arbitrum/sdk` ArbitrumNetwork object using the chain id of the Arbitrum One chain
-// const childNetwork = await getArbitrumNetwork(42161)
-// const ethBridger = new EthBridger(childNetwork)
+// get the `@arbitrum/sdk` ArbitrumNetwork object using the chain id of the Arbitrum One chain
+const bridger = async () => {
+    const childNetwork = await getArbitrumNetwork(42161)
+const ethBridger = new EthBridger(childNetwork)
 
-// const ethDepositTxResponse = await ethBridger.deposit({
-//   amount: utils.parseEther('23'),
-//   parentSigner, // an ethers v5 signer connected to mainnet ethereum
-//   childProvider, // an ethers v5 provider connected to Arbitrum One
-// })
+const ethDepositTxResponse = await ethBridger.deposit({
+  amount: paseE('23'),
+  parentSigner, // an ethers v5 signer connected to mainnet ethereum
+  childProvider, // an ethers v5 provider connected to Arbitrum One
+})
 
-// const ethDepositTxReceipt = await ethDepositTxResponse.wait()
-// console.log(ethDepositTxReceipt)
+return  await ethDepositTxResponse.wait()
+}
